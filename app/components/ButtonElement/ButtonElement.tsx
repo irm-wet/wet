c793ab376e370abc4bef8dde7133dd58268098d9
cdd <cdd@irm.kr>
2023-11-13 18:49:23 +0900

    Fix: Deploy Set

--- app/components/ButtonElement/ButtonElement.tsx
+++ app/components/ButtonElement/ButtonElement.tsx
@@ -1,6 +1,11 @@
-'use client';
+import React from 'react';
 
-const ButtonElement = ({ text, handleButtonClick }) => {
+interface ButtonElementProps {
+  text: string;
+  handleButtonClick: () => void;
+}
+
+const ButtonElement: React.FC<ButtonElementProps> = ({ text, handleButtonClick }) => {
   return (
     <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={handleButtonClick}>
       {text}
